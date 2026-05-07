a6a9b36d91ea1ddcf692fa6afc228c63d98b2391
damngocson05 <k235480106061@tnut.edu.vn>
2026-05-08 05:10:27 +0800

    Add ImageUrl field to Tour model
    Added ImageUrl field to the Tour model.

--- Sonnohu/models/tour.py
+++ Sonnohu/models/tour.py
@@ -1,11 +1,12 @@
 from models.db import db
 
+
 class Category(db.Model):
     __tablename__ = 'Categories'
     CategoryID = db.Column(db.Integer, primary_key=True)
     CategoryName = db.Column(db.String(100), nullable=False)
-    # Giúp lấy nhanh danh sách tour từ một category
     tours = db.relationship('Tour', backref='category', lazy=True)
+
 
 class Tour(db.Model):
     __tablename__ = 'Tours'
@@ -17,3 +18,4 @@ class Tour(db.Model):
     Duration = db.Column(db.String(50))
     DepartureLocation = db.Column(db.String(100))
     ImageThumbnail = db.Column(db.String(255))
+    ImageUrl = db.Column(db.String(255))
